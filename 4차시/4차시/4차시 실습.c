276a8dea123dba478602d32968cd19e1e7a52d88
gyeong33 <jungyg9765@gmail.com>
2026-09-17 16:04:43 +0900

    4주차 2차시

--- 4차시/4차시/4차시 실습.c
+++ 4차시/4차시/4차시 실습.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main() {
+	/*
 	float height_cm, weight, bmi;
 
 	// 사용자 입력
@@ -17,5 +18,37 @@ int main() {
 
 	// 결과 출력
 	printf("당신의 체질량 지수 : %.2f\n", bmi);
+	
+
+	int x = 100, y;
+	y = x++;
+
+	printf("x = %d, y = %d\n",x,y);
+	
+
+	int a = 10, b = 14;
+
+	if (a > b)
+		printf("a는 b보다 큽니다.\n");
+	else
+		printf("a는 b보다 크지 않습니다.\n");
+	
+
+	int x, y;
+	printf("두 정수를 입력하시오:");
+	scanf("%d%d", &x, &y);
+
+	printf("%d && %d의 결과값: %d\n", x, y, x && y);
+	printf("%d || %d의 결과값: %d\n", x, y, x || y);
+	printf("!%d의 결과값: %d\n", x, !x);
+	*/
+
+	int x = 20;
+
+	printf("x = %d입니다.\n", x);
+	x += 10;
+	printf("x += 10; 의 결과 x = %d\n",x);
+	x -= 10;
+	printf("x -= 10; 의 결과 x = %d\n",x);
 
 }
